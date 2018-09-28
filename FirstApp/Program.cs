8f197feb8186232d8c5b838e46b21fa83101db34
Jarosław Wójcik <jaroslavwojcik@gmail.com>
2018-09-28 21:28:18 +0200

    First commit

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace FirstApp
 {
@@ -6,7 +7,35 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            do
+            {
+                Console.WriteLine("Please write something below");
+                var query = Console.ReadLine();
+                var option = InputChecker(query);
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Hi!");
+                        break;
+                    case 2:
+                        Console.WriteLine("I am not a calculator mate");
+                        break;
+                    case 3:
+                        Console.WriteLine(DateTime.Now.ToShortDateString());
+                        break;
+                }
+                Console.WriteLine("Press any key to continue or escape to quit");
+                
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+
+        static int InputChecker(string input)
+        {
+
+            if (input.Any(char.IsDigit)) return 2;
+            else if (input == "Date") return 3;
+            else return 1;
         }
     }
 }
